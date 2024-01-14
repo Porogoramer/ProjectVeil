2b49b4c842c8fc6178dbf36294fd7f711aba1a17
Brochua <brochua17@gmail.com>
2024-01-14 01:03:09 -0500

    Added tree with billboarding always faces player

--- Assets/Scripts/SpriteBillboard.cs
+++ Assets/Scripts/SpriteBillboard.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class SpriteBillboard : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(0, Camera.main.transform.rotation.y, 0);
+        transform.rotation = Quaternion.Euler(0, player.transform.rotation.eulerAngles.y, 0);
     }
 }
